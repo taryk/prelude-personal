f4b1a98644e2fc7cfe3a1a3259f45ec33087257e
Taras Iagniuk <mrtaryk@gmail.com>
2017-04-16 01:31:55 +0200

    Add highlight-symbol package.

---
+++ init-highlight-symbol.el
@@ -0,0 +1,7 @@
+(prelude-require-packages '(highlight-symbol))
+
+(require 'highlight-symbol)
+(global-set-key [(control f3)] 'highlight-symbol)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+(global-set-key [(meta f3)] 'highlight-symbol-query-replace)
