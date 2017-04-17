a593a82a1f2673442357bfd24bb684e7ec63a9d8
Taras Iagniuk <mrtaryk@gmail.com>
2017-04-17 18:02:31 +0200

    Cperl array/hash faces got updated.

--- custom.el
+++ custom.el
@@ -11,4 +11,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(cperl-array-face ((t (:foreground "green" :weight bold))))
+ '(cperl-hash-face ((t (:foreground "orange red" :weight bold)))))
