acf6c40b5b538d52c605e686b812faf7e45dc64e
Taras Iagniuk <mrtaryk@gmail.com>
2017-04-17 17:57:28 +0200

    Add some useful functions.

---
+++ init-defuns.el
@@ -0,0 +1,22 @@
+(defun file-string (file)
+  "Read the contents of a file and return as a string."
+  (with-temp-buffer
+    (insert-file-contents file)
+    (buffer-string)))
+
+;; Finds duplicate lines and keep only the first occurrence
+(defun uniquify-all-lines-region (start end)
+  "Find duplicate lines in region START to END keeping first occurrence."
+  (interactive "*r")
+  (save-excursion
+    (let ((end (copy-marker end)))
+      (while
+          (progn
+            (goto-char start)
+            (re-search-forward "^\\(.*\\)\n\\(\\(.*\n\\)*\\)\\1\n" end t))
+        (replace-match "\\1\n\\2")))))
+
+(defun uniquify-all-lines-buffer ()
+  "Delete duplicate lines in buffer and keep first occurrence."
+  (interactive "*")
+  (uniquify-all-lines-region (point-min) (point-max)))
